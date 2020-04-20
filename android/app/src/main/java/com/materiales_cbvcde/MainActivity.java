6753e74c91382cc759f6059b0c862eede414206f
Juan Baez <baez.juann@gmail.com>
2020-04-20 16:53:10 -0400

    Initial commit, react native

---
+++ android/app/src/main/java/com/materiales_cbvcde/MainActivity.java
@@ -0,0 +1,15 @@
+package com.materiales_cbvcde;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "materiales_cbvcde";
+  }
+}
