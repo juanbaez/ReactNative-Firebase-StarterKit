e70d559a99aee7f62cb5af34fb67ee5310125ecb
Juan Baez <baez.juann@gmail.com>
2020-05-06 23:50:00 -0400

    Se agrega funcionalidad de login con cuenta de google

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,31 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {Text, View, TouchableOpacity, StatusBar, Image} from 'react-native';
 import {mainStyles, loginStyles} from '@styles/styles';
 import MyTextInput from '@components/MyTextInput';
 import MyButton from '@components/MyButton';
 import color from '@styles/colors';
 import {UsuarioContext} from '@context/UsuarioContext';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-community/google-signin';
+
 
 export default function LoginScreen(props) {
   const [login, loginAction] = useContext(UsuarioContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [hidePassword, setHidePassword] = useState(false);
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId: '159580727217-tn5slf821tq407f9e57lipoui8f7em45.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      //hostedDomain: '', // specifies a hosted domain restriction
+      //loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      //accountName: '', // [Android] specifies an account name on the device that should be used
+      //iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    });
+  }, []);
 
   return (
     <View style={[mainStyles.container, {padding: 50}]}>
@@ -21,6 +36,11 @@ export default function LoginScreen(props) {
           style={{height: 250, width: 250}}
         />
       </View>
+      <GoogleSigninButton
+          style={{ width: 192, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={() => _googleSingIn()}/>
       <MyTextInput
         keyboardType="email-address"
         placeholder="E-mail"
@@ -58,6 +78,28 @@ export default function LoginScreen(props) {
       </View>
     </View>
   );
+
+  
+
+  async function _googleSingIn(){
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo)
+      //this.setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+      console.log(error)
+    }
+  }
 
   function iniciarSesion() {
     loginAction({
