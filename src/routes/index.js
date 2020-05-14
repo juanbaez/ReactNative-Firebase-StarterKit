8946aaa9341baa2e959274bb9bc4d55a946f2a18
Juan Baez <baez.juann@gmail.com>
2020-05-14 11:20:58 -0400

    Se crean pantallas parciales de login-singup y resetpassword con su correspondientes routes y una configuracion de firebase(por el momento con un warning por parte de firebase)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Scene, Tabs, Stack } from 'react-native-router-flux';
-import { Icon } from 'native-base';
-import DefaultProps from '../constants/navigation';
-import AppConfig from '../constants/config';
+import React from 'react'
+import { Scene, Tabs, Stack } from 'react-native-router-flux'
+import { Icon } from 'native-base'
+import DefaultProps from '../constants/navigation'
+import AppConfig from '../constants/config'
 
-import { ArticlesForm, ArticlesList, ArticlesSingle } from '../containers';
+import { ArticlesForm, ArticlesList, ArticlesSingle } from '../containers'
 
-import AboutComponent from '../components/About';
+import SignUpContainer from '../containers/SignUp'
+import SignUpComponent from '../components/User/SignUp'
+
+import LoginContainer from '../containers/Login'
+import LoginComponent from '../components/User/Login'
+
+import ForgotPasswordContainer from '../containers/ForgotPassword'
+import ForgotPasswordComponent from '../components/User/ForgotPassword'
+
+import UpdateProfileContainer from '../containers/UpdateProfile'
+import UpdateProfileComponent from '../components/User/UpdateProfile'
+
+import MemberContainer from '../containers/Member'
+import ProfileComponent from '../components/User/Profile'
+
+import AboutComponent from '../components/About'
 
 const Index = (
   <Stack hideNavBar>
@@ -38,16 +53,56 @@ const Index = (
         </Stack>
 
         <Stack
+          key="profile"
+          title="PROFILE"
+          icon={() => <Icon name="contact" {...DefaultProps.icons} />}
+          {...DefaultProps.navbarProps}
+        >
+          <Scene key="profileHome" component={MemberContainer} Layout={ProfileComponent} />
+          <Scene
+            back
+            key="signUp"
+            title="SIGN UP"
+            {...DefaultProps.navbarProps}
+            component={SignUpContainer}
+            Layout={SignUpComponent}
+          />
+          <Scene
+            back
+            key="login"
+            title="LOGIN"
+            {...DefaultProps.navbarProps}
+            component={LoginContainer}
+            Layout={LoginComponent}
+          />
+          <Scene
+            back
+            key="forgotPassword"
+            title="FORGOT PASSWORD"
+            {...DefaultProps.navbarProps}
+            component={ForgotPasswordContainer}
+            Layout={ForgotPasswordComponent}
+          />
+          <Scene
+            back
+            key="updateProfile"
+            title="UPDATE PROFILE"
+            {...DefaultProps.navbarProps}
+            component={UpdateProfileContainer}
+            Layout={UpdateProfileComponent}
+          />
+        </Stack>
+        {/* <Stack
           key="form"
           title="Articles Form"
           icon={() => <Icon name="contact" {...DefaultProps.icons} />}
           {...DefaultProps.navbarProps}
         >
           <Scene key="form" component={ArticlesForm} />
-        </Stack>
+        </Stack> */}
       </Tabs>
     </Scene>
   </Stack>
-);
+)
 
-export default Index;
+export default Index
